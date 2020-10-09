49656b10cd67b0a7c7510679f97d31cf97606f15
justpeterpan <peteylegend@gmail.com>
2020-10-09 15:48:27 +0200

    select positioning fixed

--- pages/index.vue
+++ pages/index.vue
@@ -194,12 +194,17 @@ select {
   background: transparent;
   background-image: url("data:image/svg+xml;utf8,<svg fill='white' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
   background-repeat: no-repeat;
+  /* background-position-x: 100%; */
+  /* background-position-y: -5px; */
   color: var(--color);
   font-size: 21px;
   line-height: normal;
   font-weight: 700;
   border: none;
-  width: 120%;
+  width: 100%;
+  appearance: none;
+  display: block;
   text-align-last: center;
+  padding-left: 20px;
 }
 </style>
