0e36b2e557fbd1db7fe0ef30ac6d02af12304093
justpeterpan <peteylegend@gmail.com>
2020-10-26 13:02:58 +0100

    loading spinner added while site is loading

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <div :class="{ loading: isLoading }"></div>
+
     <nav>
       <select
         :class="{ isDarkTheme: $colorMode.value === 'dark' }"
@@ -44,7 +46,9 @@
                   :src="require(`../assets/logos/${position.logo}.svg`)"
                   alt=""
               /></span>
-              <div v-if="isMobile" class="team_short">{{ position.logo }}</div>
+              <div v-if="isMobile" class="team_short">
+                {{ position.logo }}
+              </div>
               <div v-else class="team">{{ position.team }}</div>
             </td>
           </tr>
@@ -69,7 +73,9 @@
                   :src="require(`../assets/logos/${position.logo}.svg`)"
                   alt=""
               /></span>
-              <div v-if="isMobile" class="team_short">{{ position.logo }}</div>
+              <div v-if="isMobile" class="team_short">
+                {{ position.logo }}
+              </div>
               <div v-else class="team">{{ position.team }}</div>
             </td>
           </tr>
@@ -94,13 +100,17 @@
 <script>
 import { mapActions, mapState } from "vuex";
 
+import Dots from "../assets/dots";
+
 import baum from "../assets/baum.json";
 import matchDays from "../assets/matchDays.json";
 
 export default {
+  components: { Dots },
   data() {
     return {
-      selected: "4"
+      selected: "4",
+      isLoading: true
     };
   },
   computed: {
@@ -137,12 +147,17 @@ export default {
       "setHasToPayTotal"
     ]),
     init() {
+      console.log(this.isLoading);
       this.getPlayedMatchDays();
       this.setSelectedMatchDay(this.playedMatchDays.length);
       this.getSelectedMatchDayTable(`${this.selectedMatchDay}`);
       this.setHasToPay(`${this.selectedMatchDay}`);
       this.setHasToPayTotal();
       this.setSelected;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 700);
+      console.log(this.isLoading);
     },
     changeSelectedMatchDay(e) {
       this.setSelectedMatchDay(e.target.value);
@@ -289,8 +304,135 @@ footer {
   justify-content: center;
   align-items: center;
   position: absolute;
-  bottom: 20;
   width: 100%;
   height: 100px; /* Height of the footer */
+}
+
+/* Absolute Center Spinner */
+.loading {
+  position: fixed;
+  z-index: 999;
+  height: 2em;
+  width: 2em;
+  overflow: visible;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+/* Transparent Overlay */
+.loading:before {
+  content: "";
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 1);
+}
+
+/* :not(:required) hides these rules from IE9 and below */
+.loading:not(:required) {
+  /* hide "loading..." text */
+  font: 0/0 a;
+  color: transparent;
+  text-shadow: none;
+  background-color: transparent;
+  border: 0;
+}
+
+.loading:not(:required):after {
+  content: "";
+  display: block;
+  font-size: 10px;
+  width: 1em;
+  height: 1em;
+  margin-top: -0.5em;
+  -webkit-animation: spinner 1500ms infinite linear;
+  -moz-animation: spinner 1500ms infinite linear;
+  -ms-animation: spinner 1500ms infinite linear;
+  -o-animation: spinner 1500ms infinite linear;
+  animation: spinner 1500ms infinite linear;
+  border-radius: 0.5em;
+  -webkit-box-shadow: rgba(255, 255, 255, 0.75) 1.5em 0 0 0,
+    rgba(255, 255, 255, 0.75) 1.1em 1.1em 0 0, rgba(255, 255, 255, 0.75) 0 1.5em 0 0,
+    rgba(255, 255, 255, 0.75) -1.1em 1.1em 0 0, rgba(0, 0, 0, 0.5) -1.5em 0 0 0,
+    rgba(0, 0, 0, 0.5) -1.1em -1.1em 0 0, rgba(255, 255, 255, 0.75) 0 -1.5em 0 0,
+    rgba(255, 255, 255, 0.75) 1.1em -1.1em 0 0;
+  box-shadow: rgba(0, 0, 0, 0.75) 1.5em 0 0 0,
+    rgba(255, 255, 255, 0.75) 1.1em 1.1em 0 0, rgba(255, 255, 255, 0.75) 0 1.5em 0 0,
+    rgba(255, 255, 255, 0.75) -1.1em 1.1em 0 0, rgba(255, 255, 255, 0.75) -1.5em 0 0 0,
+    rgba(255, 255, 255, 0.75) -1.1em -1.1em 0 0, rgba(255, 255, 255, 0.75) 0 -1.5em 0 0,
+    rgba(255, 255, 255, 0.75) 1.1em -1.1em 0 0;
+}
+
+/* Animation */
+
+@-webkit-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-moz-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-o-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 }
 </style>
